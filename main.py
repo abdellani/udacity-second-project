abfea923193914d15f0370bc53de79f534457ec6
abdellani <abdellani@gmail.com>
2019-09-18 14:24:45 +0100

    move sessions management into a seperated file

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from flask_wtf.csrf import CSRFProtect
 from forms import Form, RegistrationForm, LoginForm
 # databases
 from database import Base, Categorie, Item, User
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
 from dbm import db
 # Login
 from flask_github import GitHub
@@ -18,6 +16,7 @@ from loginmanager import login_manager
 from categories import categories_pages
 from items import items_pages
 from registrations import registrations_pages
+from sessions import sessions_pages
 
 app = Flask(__name__)
 
@@ -30,50 +29,26 @@ login_manager.init_app(app)
 csrf = CSRFProtect(app)
 github = GitHub(app)
 
+app.register_blueprint(categories_pages)
+app.register_blueprint(items_pages)
+app.register_blueprint(registrations_pages)
+app.register_blueprint(sessions_pages)
 
 @app.before_request
 def load_categories():
     if request.method == 'GET':
         g.categories = db.get_categories()
 
-app.register_blueprint(categories_pages)
-app.register_blueprint(items_pages)
-app.register_blueprint(registrations_pages)
-
-
-"""
-Resource :sessions
-"""
-@app.route('/login', methods=["GET"])
-def sessionsNew():
-    form = LoginForm()
-    return render_template('sessions/new.html', title="Login", form=form)
-
-
-@app.route('/login', methods=["POST"])
-def sessionsCreate():
-    form = LoginForm()
-    res = db.check_credentials(form.email.data, form.password.data)
-    if res is not None:
-        login_user(res)
-        flash(u'Welcome', "success")
-        return redirect(url_for("categories.index"))
-    else:
-        flash(u'User or password are wrong !', "danger")
-        return redirect(url_for("sessionsCreate"))
-
-
 @app.route('/login/github')
 def loginGithub():
     return github.authorize(scope="user")
-
 
 @app.route("/github-callback")
 @github.authorized_handler
 def authorized(oauth_token):
     if oauth_token is None:
         flash(u'Login failed', "danger")
-        return redirect(url_for("sessionsCreate"))
+        return redirect(url_for("sessions.create"))
     g.github_access_token = oauth_token
     github_user = github.get('/user')
     user = db.get_user_by_login(github_user['login'])
@@ -88,18 +63,10 @@ def authorized(oauth_token):
     flash(u'Welcome', "success")
     return redirect(url_for("categories.index"))
 
-
 @github.access_token_getter
 def token_getter():
     if g.github_access_token is not None:
         return g.github_access_token
-
-
-@app.route('/logout', methods=["POST"])
-def sessionsDestroy():
-    logout_user()
-    return redirect(url_for('sessionsNew'))
-
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8000, debug=True)
---
+++ sessions.py
@@ -0,0 +1,34 @@
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask import Blueprint,Flask, request, render_template, redirect, g, flash, url_for, session, jsonify
+from forms import Form
+from dbm import db
+from forms import Form, RegistrationForm, LoginForm
+
+sessions_pages = Blueprint('sessions',
+                        __name__,
+                        template_folder='templates')
+
+"""
+Resource :sessions
+"""
+@sessions_pages.route('/login', methods=["GET"])
+def new():
+    form = LoginForm()
+    return render_template('sessions/new.html', title="Login", form=form)
+
+@sessions_pages.route('/login', methods=["POST"])
+def create():
+    form = LoginForm()
+    res = db.check_credentials(form.email.data, form.password.data)
+    if res is not None:
+        login_user(res)
+        flash(u'Welcome', "success")
+        return redirect(url_for("categories.index"))
+    else:
+        flash(u'User or password are wrong !', "danger")
+        return redirect(url_for("sessions.create"))
+
+@sessions_pages.route('/logout', methods=["POST"])
+def destroy():
+    logout_user()
+    return redirect(url_for('sessions.new'))
